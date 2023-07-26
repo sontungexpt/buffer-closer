164d55be7456e7f91102c3eba07b66f790d41345
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-07-26 22:20:43 +0700

    docs: auto generate docs for plugin

--- lua/buffer-closer/modules/buffer.lua
+++ lua/buffer-closer/modules/buffer.lua
@@ -13,24 +13,24 @@ local M = {}
 -- This function checks whether the given buffer should be excluded from being closed based on the given exclusion
 -- criteria. The exclusion criteria include filetypes, buffer types, and filenames.
 --
--- @param bufnr number The buffer number to check.
--- @param excluded table The exclusion criteria.
--- @return boolean Whether the buffer should be excluded.
+-- @param bufnr (number): The buffer number to check.
+-- @param excluded (table): The exclusion criteria.
+-- @return boolean: Whether the buffer should be excluded.
 M.is_excluded = function(bufnr, excluded)
-	if excluded then
-		local filetype = get_buf_opt(bufnr, "filetype")
-		local buftype = get_buf_opt(bufnr, "buftype")
-		local filename = vim.fn.expand("%:t")
-		local contains = vim.tbl_contains
-		if
-			contains(excluded.filetypes, filetype)
-			or contains(excluded.buftypes, buftype)
-			or contains(excluded.filenames, filename)
-		then
-			return true
-		end
-	end
-	return false
+  if excluded then
+    local filetype = get_buf_opt(bufnr, "filetype")
+    local buftype = get_buf_opt(bufnr, "buftype")
+    local filename = vim.fn.expand("%:t")
+    local contains = vim.tbl_contains
+    if
+        contains(excluded.filetypes, filetype)
+        or contains(excluded.buftypes, buftype)
+        or contains(excluded.filenames, filename)
+    then
+      return true
+    end
+  end
+  return false
 end
 
 ---
@@ -38,8 +38,8 @@ end
 --
 -- This function checks whether the given buffer has unsaved changes.
 --
--- @param bufnr number The buffer number to check.
--- @return boolean Whether the buffer has unsaved changes.
+-- @param bufnr (number): The buffer number to check.
+-- @return boolean: Whether the buffer has unsaved changes.
 --
 M.is_unsaved_buffer = function(bufnr) return get_buf_opt(bufnr, "modified") end
 
@@ -54,8 +54,8 @@ M.is_unsaved_buffer = function(bufnr) return get_buf_opt(bufnr, "modified") end
 -- @return boolean Whether the buffer is outdated.
 --
 M.is_outdated_buffer = function(lastused_secs, retirement_minutes)
-	local now = os.time() -- in seconds
-	return now - lastused_secs > retirement_minutes * 60
+  local now = os.time() -- in seconds
+  return now - lastused_secs > retirement_minutes * 60
 end
 
 ---
@@ -65,34 +65,34 @@ end
 -- This function returns the list of buffer numbers that should be closed based on the retirement policy specified in the
 -- options. Buffers that should be excluded based on the exclusion criteria are not included in the returned list.
 --
--- @param opts table The user options.
+-- @param opts (table): The user options.
 -- @return table: The list of buffer numbers to close.
 -- @see buffer-closer.setup
 M.get_retired_bufnrs = function(opts)
-	local buffers = vim.fn.getbufinfo { buflisted = 1 }
-	if #buffers <= opts.min_remaining_buffers then return {} end
+  local buffers = vim.fn.getbufinfo { buflisted = 1 }
+  if #buffers <= opts.min_remaining_buffers then return {} end
 
-	local retired_buffers = vim.tbl_filter(function(buffer)
-		local bufnr = buffer.bufnr
-		return not M.is_unsaved_buffer(bufnr)
-			and M.is_outdated_buffer(buffer.lastused, opts.retirement_minutes)
-			and not (opts.ignore_working_windows and #buffer.windows > 0)
-			and not M.is_excluded(bufnr, opts.excluded)
-	end, buffers)
+  local retired_buffers = vim.tbl_filter(function(buffer)
+    local bufnr = buffer.bufnr
+    return not M.is_unsaved_buffer(bufnr)
+        and M.is_outdated_buffer(buffer.lastused, opts.retirement_minutes)
+        and not (opts.ignore_working_windows and #buffer.windows > 0)
+        and not M.is_excluded(bufnr, opts.excluded)
+  end, buffers)
 
-	local num_after_removing_retired = #buffers - #retired_buffers
-	-- full: 6
-	-- retired: 5
-	-- min_remaining: 4
-	-- num_after_removing_retired: 1
-	--
-	if num_after_removing_retired < opts.min_remaining_buffers then
-		table.sort(retired_buffers, function(a, b) return a.lastused < b.lastused end)
-		retired_buffers =
-			vim.list_slice(retired_buffers, 1, opts.min_remaining_buffers - num_after_removing_retired)
-	end
+  local num_after_removing_retired = #buffers - #retired_buffers
+  -- full: 6
+  -- retired: 5
+  -- min_remaining: 4
+  -- num_after_removing_retired: 1
+  --
+  if num_after_removing_retired < opts.min_remaining_buffers then
+    table.sort(retired_buffers, function(a, b) return a.lastused < b.lastused end)
+    retired_buffers =
+        vim.list_slice(retired_buffers, 1, opts.min_remaining_buffers - num_after_removing_retired)
+  end
 
-	return vim.tbl_map(function(buffer) return buffer.bufnr end, retired_buffers)
+  return vim.tbl_map(function(buffer) return buffer.bufnr end, retired_buffers)
 end
 
 ---
@@ -101,22 +101,22 @@ end
 -- This function closes the retired buffers based on the retirement policy specified in the options. Buffers that should
 -- be excluded based on the exclusion criteria are not closed.
 --
--- @param opts table The retirement policy options.
+-- @param opts (table): The retirement policy options.
 -- @see get_retired_bufnrs
 -- @see buffer-closer.setup
 M.close_retired_buffers = function(opts)
-	local retired_bufnrs = M.get_retired_bufnrs(opts)
+  local retired_bufnrs = M.get_retired_bufnrs(opts)
 
-	if #retired_bufnrs > 0 then
-		local has_notify, _ = pcall(require, "notify")
-		if has_notify then
-			vim.notify("buffer-closer: closing retired buffers")
-		else
-			print("buffer-closer: closing retired buffers")
-		end
+  if #retired_bufnrs > 0 then
+    local has_notify, _ = pcall(require, "notify")
+    if has_notify then
+      vim.notify("buffer-closer: closing retired buffers")
+    else
+      print("buffer-closer: closing retired buffers")
+    end
 
-		vim.tbl_map(function(bufnr) vim.api.nvim_buf_delete(bufnr, { force = true }) end, retired_bufnrs)
-	end
+    vim.tbl_map(function(bufnr) vim.api.nvim_buf_delete(bufnr, { force = true }) end, retired_bufnrs)
+  end
 end
 
 return M
